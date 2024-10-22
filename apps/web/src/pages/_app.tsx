e6ca923d681e18bd284e3e1bad738b5b22638736
Action <admin@actn.xyz>
2024-10-22 12:29:53 +0600

    login screen

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { PaperProvider } from "react-native-paper";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider>
+        <Component {...pageProps} />
+      </PaperProvider>
+    </QueryClientProvider>
+  );
 }
